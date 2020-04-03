5ca8eb6c9be2492a2151ee01611cb81c707b2892
Andrey Maslov <andryusha@andryusha.com>
2020-04-03 15:12:04 +0300

    Added custom webpacker source path support

--- lib/optimism/rake.rb
+++ lib/optimism/rake.rb
@@ -2,8 +2,10 @@ require "fileutils"
 
 desc "Inject some optimism into this application"
 task :"optimism:install" do
+  app_path_part = (Webpacker && Rails) ? Webpacker.config.source_path.relative_path_from(Rails.root) : "app/javascript"
+
   CHANNELS = [
-    {app_path: "app/javascript/channels/optimism_channel.js", template_path: "../templates/optimism_channel.js" },
+    {app_path: "#{app_path_part}/channels/optimism_channel.js", template_path: "../templates/optimism_channel.js" },
     {app_path: "app/channels/optimism_channel.rb", template_path: "../templates/optimism_channel.rb" },
   ]
 
